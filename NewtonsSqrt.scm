d43f6696696c0ee9581171a6a5ce207251222a13
netotz <neto.otz@hotmail.com>
2019-07-04 20:16:11 -0500

    commented code

--- NewtonsSqrt.scm
+++ NewtonsSqrt.scm
@@ -1,17 +1,23 @@
+; get n^2
 (define (square n)
     (* n n))
 
+; boolean procedure
+; true if guessed root is almost the exact root
 (define (isCloseEnough? root radicand)
-    (< (abs (- (square root) radicand)) 0.0001))
+    (< (abs (- (square root) radicand)) 0.00001))
 
+; recalculates the guessed root
 (define (newGuess root radicand)
     (/ (+ root (/ radicand root) ) 2))
 
+; decides whether to continue guessing the root or to stop
 (define (iterSqrt root radicand)
     (if (isCloseEnough? root radicand)
         root
         (iterSqrt (newGuess root radicand) radicand)))
 
+; calls the recursive function with 1 as default guess
 (define (sqrt num)
     (iterSqrt 1.0 num))
 
