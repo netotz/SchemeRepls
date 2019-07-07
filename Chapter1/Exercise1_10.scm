72756098579b3d61bfd402d3e5559056851c97cd
netotz <neto.otz@hotmail.com>
2019-07-06 19:56:10 -0500

    operations' notations added

--- Chapter1/Exercise1_10.scm
+++ Chapter1/Exercise1_10.scm
@@ -10,3 +10,22 @@
 (Ack 1 10)  ; => 1024
 (Ack 2 4)   ; => 65536
 (Ack 3 3)   ; => 65536
+
+; ↑n    Knuth's up-arrow notation
+; [n]   Hyperoperation sequence
+
+(define (f x) (Ack 0 x)) ; = 2x
+(define (g x) (Ack 1 x) ; = 2**x = 2 ↑ x = 2 [2] x
+    ; Ack = 2n if m = 0, and Ack = 2 when n = 1
+    ; x has a decrement of 1 each call
+    ; so the final return is 2 raised to x 
+    ; e.g.
+    (g 3)
+    (Ack 0 (Ack 1 2))
+    (Ack 0 (Ack 0 (Ack 1 1)))
+    (Ack 0 (Ack 0 2))
+    (Ack 0 4)
+    8
+
+(define (h x) (Ack 2 x)) ; = (tetration x 2) = 2 ↑↑ x = 2 [4] x
+(define (k x) (* 5 x x)) ; = 5x**2
