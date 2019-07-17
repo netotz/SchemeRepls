05ed8524b5c0c3ac57fd5409619028058471afc8
netotz <neto.otz@hotmail.com>
2019-07-16 22:12:50 -0500

    corrected 1.16

--- Chapter1/Exercise1_16.scm
+++ Chapter1/Exercise1_16.scm
@@ -1,9 +1,24 @@
 ;; 1.16
 ;; Successive squaring iterative process
-(define (square x)
-    (* x x))
 
-(define (succSq product counter)
+(define (evenExp product counter)
     (if (= counter 1)
         product
-        (succSq (square product) (/ counter 2))))
+        (evenExp (* product product) (/ counter 2))))
+
+; using book's hint
+(define (oddExp a b n)
+    (if (= n 0)
+        a
+        (oddExp (* a b) b (- n 1))))
+
+(define (fastExpIter state_var base exponent)
+    (cond ((= exponent 0)
+            state_var)
+        ((even? exponent)
+            (fastExpIter state_var (* base base) (/ exponent 2)))
+        (else
+            (fastExpIter (* state_var base) base (- exponent 1)))))
+
+(define (power base exponent)
+    (fastExpIter 1 base exponent))
